37c1ab1cf8fbd924caa5a53f46ab0c95f7e01968
greenmelon <suren78621@gmail.com>
2024-02-28 15:40:14 +0545

    module

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+// const express = require("express");
+// const cors = require("cors");
+import express from "express";
+import cors from "cors";
 
 let notes = [
   {
