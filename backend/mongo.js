b864f8f69e501b4af3923f004ef1a89d00b580ce
greenmelon <suren78621@gmail.com>
2024-03-03 06:00:20 +0545

    mongo db

---
+++ backend/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstackPart3:${password}@cluster0.egii1dr.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+// const note = new Note({
+//   content: "HTML is easy",
+//   important: true,
+// });
+
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   console.log(result);
+//   mongoose.connection.close();
+// });
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
