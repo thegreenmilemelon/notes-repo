af55830cdf0bd9acc1c528a380f72f43b3ac27b4
greenmelon <suren78621@gmail.com>
2024-03-05 07:15:42 +0545

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,35 @@
 const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
+const app = express();
 require("dotenv").config();
+
 const Note = require("./models/note");
 
-const app = express();
+let notes = [];
 
-app.use(express.json());
+app.use(express.static("dist"));
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+const cors = require("cors");
 
 app.use(cors());
-app.use(
-  morgan(
-    ":method :url :status :res[content-length] - :response-time ms :body :date[web] :remote-addr"
-  )
-);
-app.use(express.static("dist"));
+
+app.use(express.json());
+app.use(requestLogger);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
 app.get("/api/notes", (request, response) => {
   Note.find({}).then((notes) => {
@@ -45,9 +60,14 @@ app.get("/api/notes/:id", (request, response) => {
   });
 });
 
-morgan.token("body", (req) => {
-  return JSON.stringify(req.body);
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
 });
+
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
